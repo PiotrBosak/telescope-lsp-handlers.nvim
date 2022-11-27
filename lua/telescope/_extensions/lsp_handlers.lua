1131b5f90c5f4d7fa7be1c08d10399c755c51376
Piotr Bosak <pbk@touk.pl>
2022-11-27 16:39:24 +0100

    a

--- lua/telescope/_extensions/lsp_handlers.lua
+++ lua/telescope/_extensions/lsp_handlers.lua
@@ -129,7 +129,7 @@ function filter_imports(value)
 local function location_handler(prompt_title, opts)
 	return function(_, result1, result2, _)
     local result = get_correct_result(result1, result2)
-    opts.telescope.path_display = "truncate"
+    opts.telescope.path_display = {"hidden"}
     opts.telescope.layout_strategy = "vertical"
     opts.telescope.layout_config = {
           height = vim.o.lines, -- maximally available lines
